152ed74332bbc5f04518448819135b7758024580
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-27 20:49:31 -0230

    Update GlowstoneBlocks.java

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
@@ -8,10 +8,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 
 public final class GlowstoneBlocks {
-    public static int glowstoneDefaultLightLevel = 15;
-    public static final Block POLISHED_GLOWSTONE = register("polished_glowstone", new Block(Block.Settings.create().luminance(glowstoneDefaultLightLevel)));
-    public static final Block POLISHED_GLOWSTONE_BRICKS = register("polished_glowstone_bricks", new Block(Block.Settings.create().luminance(glowstoneDefaultLightLevel)));
-    public static final Block GLOWSTONE_BRICKS = register("glowstone_bricks", new Block(Block.Settings.create().luminance(glowstoneDefaultLightLevel)));
+    public static final Block POLISHED_GLOWSTONE = register("polished_glowstone", new Block(Block.Settings.create().luminance("15")));
+    public static final Block POLISHED_GLOWSTONE_BRICKS = register("polished_glowstone_bricks", new Block(Block.Settings.create().luminance("15")));
+    public static final Block GLOWSTONE_BRICKS = register("glowstone_bricks", new Block(Block.Settings.create().luminance("15")));
     
     private static <T extends Block> T register(String path, T block) {
         Registry.register(Registries.BLOCK, Identifier.of("glowstone-extended", path), block);
