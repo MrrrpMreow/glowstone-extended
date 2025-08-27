01b42ba3ddb460c7e7b47d7ddcecc6405f284c64
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-27 20:00:48 -0230

    WORK DAMNETH THOU

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
@@ -9,6 +9,10 @@ import net.minecraft.item.BlockItem;
 
 public final class GlowstoneBlocks {
     public static final Block POLISHED_GLOWSTONE = register("polished_glowstone", new Block(Block.Settings.create().strength(4.0f)));
+    public static final Block POLISHED_GLOWSTONE_BRICKS = register("polished_glowstone_bricks", new Block(Block.Settings.create().strength(4.0f)));
+    public static final Block GLOWSTONE_BRICKS = register("glowstone_bricks", new Block(Block.Settings.create().strength(4.0f)));
+    //public static final Block POLISHED_GLOWSTONE = register("polished_glowstone", new Block(Block.Settings.create().strength(4.0f)));
+    //public static final Block POLISHED_GLOWSTONE = register("polished_glowstone", new Block(Block.Settings.create().strength(4.0f)));
     
     private static <T extends Block> T register(String path, T block) {
         Registry.register(Registries.BLOCK, Identifier.of("glowstone-extended", path), block);
