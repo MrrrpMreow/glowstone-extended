e4a67ad9813c95e9d7f4ff3cd89f8a358071ed17
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-29 12:37:04 -0230

    missing imports as per usual

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneCreativeGUI.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneCreativeGUI.java
@@ -5,6 +5,8 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 
