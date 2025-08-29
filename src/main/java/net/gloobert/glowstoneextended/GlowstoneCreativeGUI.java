bc05e374010e21a783b09bc6763e1238610f78c5
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-29 12:48:49 -0230

    Full Ass Creative Inventory

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneCreativeGUI.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneCreativeGUI.java
@@ -4,6 +4,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
@@ -17,7 +18,28 @@ public class GlowstoneCreativeGUI {
         .icon(() -> new ItemStack(GlowstoneBlocks.POLISHED_GLOWSTONE))
         .displayName(Text.translatable("itemGroup.glowstone-extended.glowstone_extended_creative"))
         .entries((context, entries) -> {
+            entries.add(items.GLOWSTONE);
+            entries.add(GlowstoneBlocks.SOUL_GLOWSTONE);
+            entries.add(GlowstoneBlocks.WHITE_GLOWSTONE);
+            entries.add(GlowstoneBlocks.LIGHT_GRAY_GLOWSTONE);
+            entries.add(GlowstoneBlocks.GRAY_GLOWSTONE);
+            entries.add(GlowstoneBlocks.BLACK_GLOWSTONE);
+            entries.add(GlowstoneBlocks.BROWN_GLOWSTONE);
+            entries.add(GlowstoneBlocks.RED_GLOWSTONE);
+            entries.add(GlowstoneBlocks.ORANGE_GLOWSTONE);
+            entries.add(GlowstoneBlocks.YELLOW_GLOWSTONE);
+            entries.add(GlowstoneBlocks.LIME_GLOWSTONE);
+            entries.add(GlowstoneBlocks.GREEN_GLOWSTONE);
+            entries.add(GlowstoneBlocks.CYAN_GLOWSTONE);
+            entries.add(GlowstoneBlocks.LIGHT_BLUE_GLOWSTONE);
+            entries.add(GlowstoneBlocks.BLUE_GLOWSTONE);
+            entries.add(GlowstoneBlocks.PURPLE_GLOWSTONE);
+            entries.add(GlowstoneBlocks.MAGENTA_GLOWSTONE);
+            entries.add(GlowstoneBlocks.PINK_GLOWSTONE);
             entries.add(GlowstoneBlocks.POLISHED_GLOWSTONE);
+            entries.add(GlowstoneBlocks.POLISHED_SOUL_GLOWSTONE);
+            entries.add(GlowstoneBlocks.POLISHED_GLOWSTONE_BRICKS);
+            entries.add(GlowstoneBlocks.GLOWSTONE_BRICKS);
         })
         .build());
     public static void initialize() {
