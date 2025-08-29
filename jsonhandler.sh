08242d240346ad01fb6a1c3df452afce7af81a1f
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-28 23:31:37 -0230

    forgot item models dont go in subfolders

--- jsonhandler.sh
+++ jsonhandler.sh
@@ -3,4 +3,4 @@ echo Usage: jsonhandler.sh blockname \(optional blockpath ending in /\)
 
 printf "{\n  \"variants\": {\n    \"\": { \"model\": \"glowstone-extended:block/$2$1\" }\n  }\n}" > ./src/main/resources/assets/glowstone-extended/blockstates/$1.json
 printf "{\n  \"parent\": \"block/cube_all\",\n  \"textures\": {\n    \"all\": \"glowstone-extended:block/$2$1\"\n  }\n}" > ./src/main/resources/assets/glowstone-extended/models/block/$2$1.json
-printf "{\n  \"parent\": \"glowstone-extended:block/$2$1\"\n}" > ./src/main/resources/assets/glowstone-extended/models/item/$2$1.json
+printf "{\n  \"parent\": \"glowstone-extended:block/$2$1\"\n}" > ./src/main/resources/assets/glowstone-extended/models/item/$1.json
