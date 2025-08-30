d840b4439ebd4e92c47ddb8271789ab2baf4b88a
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-29 23:17:29 -0230

    AxeItem

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
@@ -14,6 +14,7 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.world.World;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.item.AxeItem;
 
 
 public final class GlowstoneBlocks {
