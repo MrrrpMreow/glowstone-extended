982f778cd0addabf9eb74e2d12e61efe99839a33
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-08-31 22:38:24 -0230

    Copy line 11 to line 12 and replace ; with s;

--- src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
+++ src/main/java/net/gloobert/glowstoneextended/GlowstoneBlocks.java
@@ -9,6 +9,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.item.AxeItem;
